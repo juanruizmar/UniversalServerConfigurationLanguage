86605d4dfcb8bbd48bbcf128a401cdc8c4eee0d2
juanruizmar <101345924+juanruizmar@users.noreply.github.com>
2024-05-28 15:22:58 +0200

    Update CalcLexer.py
    Lexer terminado

--- CalcLexer.py
+++ CalcLexer.py
@@ -1,44 +1,46 @@
 from sly import Lexer
-from sly import Parser
-
-import sys
 
 class CalcLexer(Lexer):
 	tokens = {
-		OpenBracket, CloseBracket, Semicolon, Extension, At, Dot, Int, Cadena, Bool, Quotes, Domain,
-
-		BaseNow, BaseOther, TimeUnit, FileType, Encoding, TestString, CP, Pattern, Substitution,
+		OPEN_BRACKET, CLOSE_BRACKET, SEMICOLON, EXTENSION, AT, DOT, INT, CADENA, BOOL, QUOTES, DOMAIN,
+		OPTIONS_VAR, ERRORS, MULTIPROC_OPTIONS, 
+		BASE_NOW, BASE_OTHER, TIME_UNIT, FILE_TYPE, ENCODING, 
+		TEST_STRING, PATTERN, SUBSTITUTION, CP_BASIC_AUX, CP_CIERRES, CP_CHAR
 	}
 
 	ignore = r' |\t'
 
-	OpenBracket = '{'
-	CloseBracket = '}'
+	OPEN_BRACKET = '{'
+	CLOSE_BRACKET = '}'
+	SEMICOLON = ';'
+	EXTENSION = r'html|com'
 
-	Semicolon = ';'
-	Extension = r'html|com'	# Todas las extensiones válidas 
+	AT = '@'
+	DOT = '.'
+	INT = r'\d+'
+	CADENA = r'\w+'
+	BOOL = r'On|Off'
+	QUOTES = '"'
+	DOMAIN = r'gmail'	# Una lista de dominios válidos (por lo pronto sólo uno)
 
-	At = '@'
-	Dot = '.'
-	Int = r'\d+'
-	Cadena = r'\w+'
-	Bool = r'On|Off'
-	Quotes = '"'
-	Domain = 'LoL xD'	# Una lista de dominios válidos
-	BaseNow = 'now'
-	BaseOther = r'access|remote'
-	TimeUnit = r'Year|Month|Day|Hour|Minute|Second'
-	FileType = r'text|image|video'
-	Encoding = r'html|gif|H.264'
-	TestString = r'REQUEST_URI|REQUEST_FILENAME|QUERY_STRING|REMOTE_ADDR'
+	OPTIONS_VAR = r'none|all|indexes|followsymlinks|execcgi'
+	ERRORS = r'400|401|403|404|500|503'
+
+	MULTIPROC_OPTIONS = r'prefork|worker|event'
+
+	BASE_NOW = 'now'
+	BASE_OTHER = r'access|remote'
+	TIME_UNIT = r'Year|Month|Day|Hour|Minute|Second'
+	FILE_TYPE = r'text|image|video'
+	ENCODING = r'html|gif|H.264'
+
+	TEST_STRING = r'REQUEST_URI|REQUEST_FILENAME|QUERY_STRING|REMOTE_ADDR'
+	PATTERN = '.\'(\'.\')\'.*'
+	SUBSTITUTION = '$1'
 
 #	Una gramática para el CP
-	CondPattern = r'CPInner|CPInner\$|\^CPInner|\^CPInner\$'
-	CPInner = r'CPInnerAux | CPInner CPInnerAux'
-	CPInnerAux = r'CPBasic|\! CPBasic|CPBasic CPCierres|\! CPBasic CPCierres'	# Añadir capa
-	CPBasic = r'CPBasicAux|CPBasic CPBasicAux'
-	CPBasicAux = r'\.|\\ CPChar'												# Añadir capa
+	CP_BASIC_AUX = r'\.'
+	CP_CIERRES = r'\*|\+'
+	CP_CHAR = r'\^|\$|\.|\\|\!|\*|\+'
 
-	CPCierres = r'\*|\+'
-	CPChar = r'\^|\$|\.|\\|\!|\*|\+'
-
+lexer = CalcLexer()
